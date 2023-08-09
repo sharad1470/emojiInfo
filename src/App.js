18515051cf224901a46a29e22dd6d04f1a6e2c08
sharad1470 <sharadsingh3609@gmail.com>
2023-08-09 16:35:18 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [word,setWord]=useState("yes");
+
+  const emojiDictionary={
+    "😊":"smiling",
+    "😂": "Laughter",
+    "❤":"Love",
+    "😍":"excited"
+}
+  const emojiList=Object.keys(emojiDictionary);
+
+
+  const changeHandle=(e)=>{
+
+    var inputKey=e.target.value;
+    if(inputKey in emojiDictionary)
+      setWord(emojiDictionary[inputKey]);
+    else  
+      setWord("No data in database");
+    }
+
+  const getEmojiName=(emoji)=>{
+    setWord(emojiDictionary[emoji]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className='App'>
+
+      <h1>inside Out!!!</h1>
+      <input type='text' 
+        onChange={changeHandle}></input>
+        
+      <h2>{word}</h2>
+
+      {emojiList.map((emoji, index)=>{
+         return <span
+          key={emoji}
+          onClick={()=>{getEmojiName(emoji)}}
+          style={{padding:"0.5rem" ,fontSize:"2rem",cursor:"pointer"}}
+         >{emoji}
+         </span>
+      }
+      )}
+         
+        
+      
     </div>
   );
 }
